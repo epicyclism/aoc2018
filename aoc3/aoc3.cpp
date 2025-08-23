be47740f22439bce59302e065966491a3f6e973d
Paul <paul@epicyclism.com>
2025-08-23 13:16:07 +0100

    aoc3

--- aoc3/aoc3.cpp
+++ aoc3/aoc3.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include <algorithm>
 #include <numeric>
 #include <ranges>
@@ -9,28 +10,67 @@
 #include "ctre_inc.h"
 #include "timer.h"
 
+struct claim_t
+{
+	int x_;
+	int y_;
+	int cx_;
+	int cy_;
+};
+
 auto get_input()
 {
-	return 0;
+	std::vector<claim_t> v;
+	std::string ln;
+	while(std::getline(std::cin, ln))
+	{
+		if(auto[m, x, y, cx, cy] = ctre::search<"(\\d+),(\\d+): (\\d+)x(\\d+)">(ln); m)
+			v.emplace_back(x.to_number<int>(), y.to_number<int>(), cx.to_number<int>(), cy.to_number<int>());
+		else
+			fmt::println("parse fail {}", ln);
+	}
+	return v;
 }
 
-int64_t pt1(auto const& in_addr_t)
+template<size_t S> void fill(std::vector<char>& bs, int x, int y, int cx, int cy)
 {
-	timer t("p1");
-	return 0;
+	for(auto line = 0; line < cy; ++line)
+	{
+		size_t pt = (line + y) * S + x;
+		for(auto dx = 0; dx < cx; ++dx)
+			++bs[pt + dx];
+	}
 }
 
-int64_t pt2(auto const& in)
+template<size_t S> bool test(std::vector<char>& bs, int x, int y, int cx, int cy)
 {
-	timer t("p2");
-	return 0;
+	for(auto line = 0; line < cy; ++line)
+	{
+		size_t pt = (line + y) * S + x;
+		for(auto dx = 0; dx < cx; ++dx)
+			if(bs[pt + dx] == 0 || bs[pt + dx] > 1)
+				return false;
+	}
+	return true;
+}
+
+auto pt12(auto const& in)
+{
+	timer t("p12");
+	std::vector<char> v(1024*1024);
+	for(auto& c: in)
+		fill<1024>(v, c.x_, c.y_, c.cx_, c.cy_);
+	size_t p2 = 0;
+	for(; p2 < in.size(); ++p2)
+		if(test<1024>(v, in[p2].x_, in[p2].y_, in[p2].cx_, in[p2].cy_))
+			break;
+	return std::make_pair(std::ranges::count_if(v, [](auto c){ return c > 1;}), p2 + 1); // claims start at 1
 }
 
 int main()
 {
 	auto in = get_input();
-	auto p1 = pt1(in);
-	auto p2 = pt2(in);
+	auto[p1, p2] = pt12(in);
 	fmt::println("pt1 = {}", p1);
 	fmt::println("pt2 = {}", p2);
 }
