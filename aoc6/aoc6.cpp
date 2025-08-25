9d03384ce928dac43beb3b7555ee59d72116d9ab
Paul <paul@epicyclism.com>
2025-08-25 17:38:21 +0100

    aoc6

--- aoc6/aoc6.cpp
+++ aoc6/aoc6.cpp
@@ -6,6 +6,7 @@
 #include <ranges>
 
 #include <fmt/format.h>
+#include <fmt/ranges.h>
 
 #include "ctre_inc.h"
 #include "timer.h"
@@ -15,18 +16,6 @@ struct pt
 	int x_;
 	int y_;
 };
-
-auto get_input()
-{
-	std::vector<pt> v;
-	std::string ln;
-	while(std::getline(std::cin, ln))
-		if(auto[m, x, y] = ctre::match<"(\\d+), (\\d+)">(ln); m)
-			v.emplace_back(x.to_number<int>(), y.to_number<int>());
-		else
-			fmt::println("parse fail: {}", ln);
-	return v;
-}
 
 auto scan(auto const& in)
 {
@@ -46,25 +35,92 @@ auto scan(auto const& in)
 	return std::make_pair(tl, br);
 }
 
-int64_t pt1(auto const& in)
+auto get_input()
 {
-	timer t("p1");
-	auto[tl, br] = scan(in);
-	fmt::println("({}, {}) - ({}, {})", tl.x_, tl.y_, br.x_, br.y_);
-	return 0;
+	std::vector<pt> v;
+	std::string ln;
+	while(std::getline(std::cin, ln))
+		if(auto[m, x, y] = ctre::match<"(\\d+), (\\d+)">(ln); m)
+			v.emplace_back(x.to_number<int>(), y.to_number<int>());
+		else
+			fmt::println("parse fail: {}", ln);
+	auto[tr, bl] = scan(v);
+	std::ranges::transform(v, std::ranges::begin(v), [&](auto p){ return pt(p.x_ - tr.x_, p.y_ - tr.y_);});
+	return v;
 }
 
-int64_t pt2(auto const& in)
+
+int manhattan(pt f, pt t)
 {
-	timer t("p2");
-	return 0;
+	return std::abs(f.x_ - t.x_) + std::abs(f.y_ - t.y_);
+}
+
+struct cell
+{
+	int owned_ = -1;
+	int distance_ = std::numeric_limits<int>::max();
+	int total_d_ = 0;
+};
+
+auto pt12(auto const& in)
+{
+	timer t("p12");
+	auto[tl, br] = scan(in);
+	int mxx = br.x_ + 1;
+	int mxy = br.y_ + 1;
+	std::vector<cell> vc(mxx * mxy);
+	for(int y = 0; y < mxy; ++y)
+	{
+		for(int x = 0; x < mxx; ++x)
+		{
+			int ca = y * mxx + x;
+			int t = 0;
+			for(int n = 0; n < in.size(); ++n)
+			{
+				auto d = manhattan(in[n], {x, y} );
+				t += d;
+				if( d < vc[ca].distance_)
+				{
+					vc[ca].distance_ = d;
+					vc[ca].owned_ = n;
+				}
+				else
+				if(d == vc[ca].distance_)
+					vc[ca].owned_ = -1;
+			}
+			vc[ca].total_d_ = t;
+		}
+	}
+	std::vector<bool> edge(in.size());
+	for(int x = 0; x < mxx; ++x)
+	{
+		edge[vc[x].owned_] = true;
+		edge[vc[x + (mxy - 1) * mxx].owned_] = true;
+	}
+	for(int y = 0; y < mxy; ++y)
+	{
+		edge[vc[y * mxx].owned_] = true;
+		edge[vc[y * mxx + mxy - 1].owned_] = true;
+	}
+	int p1 = 0;
+	for(int c = 0; c < in.size(); ++c)
+	{
+		if(!edge[c])
+		{
+			auto mxt = std::ranges::count_if(vc, [=](auto& cl){ return cl.owned_ == c;});
+			if(mxt > p1)
+				p1 = mxt;
+		}
+	}
+	auto thres = (in.size() < 10 ) ? 32 : 10000;
+	auto p2 = std::ranges::count_if(vc, [=](auto& cl){ return cl.total_d_ < thres;});
+	return std::make_pair(p1, p2);
 }
 
 int main()
 {
 	auto in = get_input();
-	auto p1 = pt1(in);
-	auto p2 = pt2(in);
+	auto[ p1, p2] = pt12(in);
 	fmt::println("pt1 = {}", p1);
 	fmt::println("pt2 = {}", p2);
 }
