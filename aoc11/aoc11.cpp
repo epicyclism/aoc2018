b619ca30f23589d3ad0171d73f35dcdecba8c837
Paul <paul@epicyclism.com>
2025-08-30 18:41:38 +0100

    aoc11 first pass

--- aoc11/aoc11.cpp
+++ aoc11/aoc11.cpp
@@ -1,36 +1,83 @@
 #include <iostream>
 #include <string>
-#include <algorithm>
-#include <numeric>
-#include <ranges>
+#include <tuple>
 
 #include <fmt/format.h>
-
 #include "ctre_inc.h"
+
 #include "timer.h"
 
-auto get_input()
+int power(int x, int y, int serial)
 {
-	return 0;
+	auto rack_id = x + 10;
+	auto power_level = rack_id * y + serial;
+	power_level *= rack_id;
+	power_level /= 100;
+	power_level %= 10;
+	return power_level - 5;
 }
 
-int64_t pt1(auto const& in_addr_t)
+int grid(int x, int y, int z, int serial)
+{
+	int r = 0;
+	for( int dx = 0; dx < z; ++dx)
+		for(int dy = 0; dy < z; ++dy)
+			r += power(x + dx, y + dy, serial);
+	return r;
+}
+
+auto pt1(auto in)
 {
 	timer t("p1");
-	return 0;
+	int mx = 0;
+	int my = 0;
+	int mp = 0;
+	for(int x = 1; x < 298; ++x)
+		for(int y = 1; y < 298; ++y)
+		{
+			auto p = grid(x, y, 3, in);
+			if( p > mp)
+			{
+				mp = p;
+				mx = x;
+				my = y;
+			}
+		}
+	return std::make_pair(mx, my);
 }
 
-int64_t pt2(auto const& in)
+auto pt2(auto in)
 {
 	timer t("p2");
-	return 0;
+	int mx = 0;
+	int my = 0;
+	int mp = 0;
+	int ms = 0;
+	for(int x = 1; x < 301; ++x)
+		for(int y = 1; y < 301; ++y)
+		{
+			for(int s = 1; s < std::min(301 - x, 301 - y); ++s)
+			{
+				auto p = grid(x, y, s, in);
+				if( p > mp)
+				{
+					mp = p;
+					mx = x;
+					my = y;
+					ms = s;
+				}
+			}
+		}
+	return std::make_tuple(mx, my, ms);
 }
 
-int main()
+int main(int ac, char** av)
 {
-	auto in = get_input();
+	auto in = 9798;
+	if(ac > 1)
+		in = sv_to_t<int>(av[1]);
 	auto p1 = pt1(in);
 	auto p2 = pt2(in);
-	fmt::println("pt1 = {}", p1);
-	fmt::println("pt2 = {}", p2);
+	fmt::println("pt1 = {},{}", p1.first, p1.second);
+	fmt::println("pt2 = {},{},{}", std::get<0>(p2), std::get<1>(p2), std::get<2>(p2));
 }
