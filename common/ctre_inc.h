c703a6b10a379810a08b1887cbf84ff149bf63ce
Paul <paul@epicyclism.com>
2025-08-30 16:58:22 +0100

    aoc10

--- common/ctre_inc.h
+++ common/ctre_inc.h
@@ -7,6 +7,15 @@ template<typename T> T sv_to_t ( std::string_view sv)
 	return t;
 }
 
+template<typename T> T svp_to_t ( std::string_view sv)
+{
+	T t { 0 };
+	if(sv[0] == ' ')
+		sv.remove_prefix(1);
+	std::from_chars(sv.data(), sv.data() + sv.size(), t);
+	return t;
+}
+
 template<typename T> T sv_to_t(std::string_view sv, int b)
 {
 	T t{ 0 };
